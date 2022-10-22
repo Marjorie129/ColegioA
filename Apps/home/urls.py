4dc9073e99dea399eaa7a69194fe713b23ba70b5
Marjorie129 <marjorierivera226@gmail.com>
2022-10-22 17:45:07 -0600

    Editar Datos, Filtros y Ver Datos

--- Apps/home/urls.py
+++ Apps/home/urls.py
@@ -15,22 +15,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from.views import HomeView, ListadoView, AdministradoresView, CrearEstudianteAView, CrearEstudiantePView, CrearArticuloView, CrearPublicacionView, CrearComentarioView, PublicacionesView,  ArticulosView, RegistroView, ComentariosView,Login
+from .views import HomeView, ListadoView, AdministradoresView, CrearEstudianteAView, CrearEstudiantePView, CrearArticuloView, CrearPublicacionView, CrearComentarioView, PublicacionesView,  ArticulosView, RegistroView, ComentariosView, Login, EditarEstudianteAView, EditarEstudiantePView, EditArticuloView, EditarComentarioView, EditarPublicacionView, detallesEP, detallesEA, detallesArt, detallesComent, detallesPub
 
-app_name='home'
+app_name = 'home'
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='homeapp'),
     path('listado/', ListadoView.as_view(), name='listadoapp'),
-    path('administradores/', AdministradoresView.as_view(), name='adminapp'),    
+    path('editarEP/<int:pk>', EditarEstudiantePView.as_view(), name='editarEPapp'),
+    path('administradores/', AdministradoresView.as_view(), name='adminapp'),
+    path('editarEA/<int:pk>', EditarEstudianteAView.as_view(), name='editarEAapp'),
     path('crearEstA/', CrearEstudianteAView.as_view(), name='crearEstAapp'),
     path('crearEstP/', CrearEstudiantePView.as_view(), name='crearEstPapp'),
     path('Art/', ArticulosView.as_view(), name='acercaDeapp'),
+    path('editArt/<int:pk>', EditArticuloView.as_view(), name='editarArtapp'),
     path('crearArt/', CrearArticuloView.as_view(), name='crearArtapp'),
     path('crearPub/', CrearPublicacionView.as_view(), name='crearPubapp'),
+    path('editPub/<int:pk>', EditarPublicacionView.as_view(), name='editarPubapp'),
     path('crearCome/', CrearComentarioView.as_view(), name='crearComeapp'),
     path('verPub/', PublicacionesView.as_view(), name='Pubapp'),
     path('registro/', RegistroView.as_view(), name='RegistroApp'),
     path('comentarios/', ComentariosView.as_view(), name='verComentapp'),
+    path('editComent/<int:pk>', EditarComentarioView.as_view(), name='editarComentapp'),
+    path('detallesEP/<int:pk>', detallesEP.as_view(), name='detallesEPapp'),
+    path('detallesEA/<int:pk>', detallesEA.as_view(), name='detallesEAapp'),
+    path('detallesArt/<int:pk>', detallesArt.as_view(), name='detallesArtapp'),
+    path('detallesComent/<int:pk>', detallesComent.as_view(), name='detallesComentapp'),
+    path('detallesPub/<int:pk>', detallesPub.as_view(), name='detallesPubapp'),
     path('', Login, name='login'),
 ]
--- Apps/home/views.py
+++ Apps/home/views.py
@@ -1,7 +1,8 @@
 from io import UnsupportedOperation
 from msilib.schema import ListView
+from multiprocessing import connection
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, UpdateView, DetailView
 from .forms import ArticuloForm, ComentarioForm, EstudiantePForm, EstudianteAForm, PublicacionForm, ComentarioForm, RegistroForm
 from django.urls import reverse_lazy
 from .models import EstudianteAutorizaciones, EstudiantePublicaciones, Publicaciones, Usuario, ArticuloEstudiante, ComentariosEstud
@@ -12,6 +13,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate
 # Create your views here.
+from django.db import connection
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -20,7 +22,12 @@ class ListadoView(generic.ListView):
     template_name = 'listado.html'
     model = EstudiantePublicaciones
     def get_queryset(self):
-     return EstudiantePublicaciones.objects.all()
+     pNombre = self.request.GET.get('nombre')
+     if(pNombre):
+        return EstudiantePublicaciones.objects.filter(nombre__icontains=pNombre)
+     else:
+        return EstudiantePublicaciones.objects.all()
+
 
 def CrearPublicacion (request):
     assert isinstance(request, HttpRequest)
@@ -36,7 +43,12 @@ class AdministradoresView(generic.ListView):
     template_name = 'administradores.html'
     model = EstudianteAutorizaciones
     def get_queryset(self):
-     return EstudianteAutorizaciones.objects.all()
+     aNombre = self.request.GET.get('nombre')
+     if(aNombre):
+        return EstudianteAutorizaciones.objects.filter(nombre__icontains=aNombre)
+     else:
+        return EstudianteAutorizaciones.objects.all()
+
 
 def RealizarAutorizacion (request):
     assert isinstance(request, HttpRequest)
@@ -52,7 +64,11 @@ class PublicacionesView(generic.ListView):
     template_name = 'Publicaciones.html'
     model = Publicaciones
     def get_queryset(self):
-     return Publicaciones.objects.all()
+     pPub = self.request.GET.get('id')
+     if(pPub):
+        return Publicaciones.objects.filter(publica=pPub)
+     else:
+        return Publicaciones.objects.all()
 
 def RealizarPublicacion (request):
     assert isinstance(request, HttpRequest)
@@ -78,8 +94,20 @@ class CrearEstudianteAView(CreateView):
     form_class = EstudianteAForm
     success_url = reverse_lazy('home:adminapp')
 
+class EditarEstudianteAView(UpdateView):
+    template_name = 'editarEstudianteA.html'
+    model=EstudianteAutorizaciones
+    form_class = EstudianteAForm
+    success_url = reverse_lazy('home:adminapp')
+
 class CrearEstudiantePView(CreateView):
     template_name = 'crearEstudianteP.html'
+    form_class = EstudiantePForm
+    success_url = reverse_lazy('home:listadoapp')
+
+class EditarEstudiantePView(UpdateView):
+    template_name = 'editarEstudianteP.html'
+    model=EstudiantePublicaciones
     form_class = EstudiantePForm
     success_url = reverse_lazy('home:listadoapp')
 
@@ -88,13 +116,31 @@ class CrearArticuloView(CreateView):
     form_class = ArticuloForm
     success_url = reverse_lazy('home:acercaDeapp')
 
+class EditArticuloView(UpdateView):
+    template_name = 'editarArticulo.html'
+    model=ArticuloEstudiante
+    form_class = ArticuloForm
+    success_url = reverse_lazy('home:acercaDeapp')
+
 class CrearPublicacionView(CreateView):
     template_name = 'crearPublicacion.html'
     form_class = PublicacionForm
     success_url = reverse_lazy('home:Pubapp')
 
+class EditarPublicacionView(UpdateView):
+    template_name = 'editarPublicacion.html'
+    model=Publicaciones
+    form_class = PublicacionForm
+    success_url = reverse_lazy('home:Pubapp')
+
 class CrearComentarioView(CreateView):
     template_name = 'crearComentario.html'
+    form_class = ComentarioForm
+    success_url = reverse_lazy('home:verComentapp')
+
+class EditarComentarioView(UpdateView):
+    template_name = 'editarComentario.html'
+    model = ComentariosEstud
     form_class = ComentarioForm
     success_url = reverse_lazy('home:verComentapp')
 
@@ -121,7 +167,11 @@ class ArticulosView(generic.ListView):
    template_name = 'acercaDe.html'
    model = ArticuloEstudiante
    def get_queryset(self):
-     return ArticuloEstudiante.objects.all()
+     aTitulo = self.request.GET.get('titulo')
+     if(aTitulo):
+        return ArticuloEstudiante.objects.filter(titulo__icontains=aTitulo)
+     else:
+        return ArticuloEstudiante.objects.all()
 
 def MostrarArticulo (request):
     assert isinstance(request, HttpRequest)
@@ -137,7 +187,11 @@ class ComentariosView(generic.ListView):
    template_name = 'comentarios.html'
    model = ComentariosEstud
    def get_queryset(self):
-     return ComentariosEstud.objects.all()
+     aComenta = self.request.GET.get('comenta')
+     if(aComenta):
+        return ComentariosEstud.objects.filter(comentario__icontains=aComenta)
+     else:
+        return ComentariosEstud.objects.all()
 
 def MostrarArticulo (request):
     assert isinstance(request, HttpRequest)
@@ -151,3 +205,23 @@ def MostrarArticulo (request):
 
 class UsuariosView(TemplateView):
     template_name = 'home.html'
+
+class detallesEP(DetailView):
+    template_name ='detallesEP.html'
+    queryset = EstudiantePublicaciones.objects.all()
+
+class detallesEA(DetailView):
+    template_name ='detallesEA.html'
+    queryset = EstudianteAutorizaciones.objects.all()
+
+class detallesArt(DetailView):
+    template_name ='detallesArt.html'
+    queryset = ArticuloEstudiante.objects.all()
+
+class detallesComent(DetailView):
+    template_name ='detallesComent.html'
+    queryset = ComentariosEstud.objects.all()
+
+class detallesPub(DetailView):
+    template_name ='detallesPub.html'
+    queryset = Publicaciones.objects.all()
